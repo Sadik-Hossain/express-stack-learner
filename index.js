65897c9e4be7e8ebc5986ea2a481d226380bd44d
sadik <po480373@gmai.com>
2022-07-18 20:45:04 +0600

    request obj method explored

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/pages", (req, res) => {
+  //console.log(req.url); // shows which endpoint user hit on the server
   //* fs module read from the specified path and take a callback fn(err,data)
   //* data:buffer is chunk of binary code
   fs.readFile("./pages/index.html", (err, data) => {
@@ -39,11 +40,20 @@ app.get("/about", (req, res) => {
   });
 });
 app.get("/help", (req, res) => {
+  //console.log(req.app); // shows the whole express app
+  // console.log(req.hostname); // to check host name :localhost
+  // console.log(req.ip); // to check ip address
+  // console.log(req.method); // for checking methods
+  // console.log(req.originalUrl); // to see full url
+  // console.log(req.get("content-type")); // ki type er header asce
+  // console.log(req.get("accept")); // ki type er header asce
+
   fs.readFile("./pages/help.html", (err, data) => {
     if (err) {
       console.log(`error`, err);
       res.send(`<h1>something went wrong</h1>`);
     } else {
+      console.log(res.headersSent);
       res.write(data);
       res.end();
     }
