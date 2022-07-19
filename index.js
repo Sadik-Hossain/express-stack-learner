9c8607ba6945405eb81a032f6cfa91c7de3caf3f
sadik <po480373@gmai.com>
2022-07-19 12:29:49 +0600

    middleware exploresd

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
+
+//* ------------ middleware -----------------
+//* ekhane direct app er sathe middleware use kora hyece, fole era global middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+//* custom global middleware
+app.use(globalM); // we dont have to call fn. cz, we maintained signature.
+// app.use(globalC()); // if we used like in line: 125
+//* ---------------------------------------------------
 
 app.get("/", (req, res) => {
   //   res.send("hello world");
@@ -53,13 +66,81 @@ app.get("/help", (req, res) => {
       console.log(`error`, err);
       res.send(`<h1>something went wrong</h1>`);
     } else {
-      console.log(res.headersSent);
+      // console.log(res.headersSent); // help us measure time and know if response sent or not
       res.write(data);
       res.end();
+      // console.log(res.headersSent);
+      // res.attachement(`filename`) // for attaching file
+      // res.append() // for combining http headers
+      // res.cookies() // for generating cookies
+      // res.clearCookies() // for clearing cooking
+      // res.download("./pages/dsa-cheatsheet.pdf"); // for dowloadable
+      // res.format({}) // for muliple content type jnno vinno vinno res pathate pari
     }
   });
 });
+app.get("/local", localM, (req, res) => {
+  res.send(`<h1>I am local route</h1>`);
+});
 //* server start
 app.listen(4000, () => {
-  console.log(`server is litening`);
+  console.log(`server is listening`);
 });
+
+/** responsibilities of a middleware
+ * - handle common tasks
+ * - requests logs
+ * - filter request
+ * - modify or reshape request
+ * - validate request body
+ * - authenticate/ authorize request
+ * - add additional details to req body
+ * - response bad request
+ * - pass req to next middleware or response handler
+ */
+// ------------------------------------------------------
+/** non-global/local middleware usage
+ * app.get("/route", cors(),(req,res)=>{...})
+ * app.get("/routes", [cors(),cors(),cors()...],(req,res)=>{...})
+ */
+//* -------------- middleware signature -----------------
+/**
+ * function handler (){
+ * - read req obj
+ * - prcess req
+ * - response back with a result
+ * }
+ *
+ * function middleware(req,res,next){
+ *
+ * next() // next call na krle system hang krbe
+ * }
+ */
+//* if everything seems ok controller will call response methods
+//* if everything seems ok middleware will call next
+
+//* -------------- custom global middleware -------------------
+function globalM(req, res, next) {
+  console.log(`${req.method}-${req.url}`);
+  console.log(`I am global middleware`);
+
+  //* req process, if bad req, return res. to prevent it from going to controller (actual response based on buisness logic)
+  if (req.query.bad) {
+    //* localhost:4000/?bad=true
+    return res.status(400).send(`Bad request`);
+  }
+  next(); // if we dont pass server will hang, client loading hoitei thakbe
+}
+// function globalC() {
+//   return function globalM(req, res, next) {
+//     console.log(`${req.method}-${req.url}`);
+//     console.log(`I am global middleware`);
+//     next(); // if we dont pass server will hang, client loading hoitei thakbe
+//   };
+// }
+
+//* ------------- custom local middleware -----------------------
+function localM(req, res, next) {
+  console.log(`Im a local middlewares`);
+  next();
+}
