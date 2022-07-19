ce05eccebbb4c1dc1d63d79162afc9fc3bbe12eb
sadik <po480373@gmai.com>
2022-07-19 12:57:21 +0600

    router explored

--- index.js
+++ index.js
@@ -3,85 +3,27 @@ const fs = require("fs");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
-
+const router = express.Router();
+//* router express theke extract kora holo
+//* ebar shob app. ke router dara replace kore dibo . then error show krbe client side e " cannot get/ ". eta basically router ekta middleware se jnno app ke bujhate hobe-> app.use(router)
 //* ------------ middleware -----------------
 //* ekhane direct app er sathe middleware use kora hyece, fole era global middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
+
+//* router as middleware
+// app.use(router);
+app.use(require("./router.js"));
+
 //* custom global middleware
 app.use(globalM); // we dont have to call fn. cz, we maintained signature.
 // app.use(globalC()); // if we used like in line: 125
 //* ---------------------------------------------------
 
-app.get("/", (req, res) => {
-  //   res.send("hello world");
-  //   res.attachment() //for pdf or any other file
-  //   res.json({
-  //     message: "hello there!",
-  //   });
-  res.send(`<h1>hello from home</h1>`);
-});
+//* app.get ke router.get dara replace korlam
 
-app.get("/pages", (req, res) => {
-  //console.log(req.url); // shows which endpoint user hit on the server
-  //* fs module read from the specified path and take a callback fn(err,data)
-  //* data:buffer is chunk of binary code
-  fs.readFile("./pages/index.html", (err, data) => {
-    if (err) {
-      console.log(`Error`, err);
-      res.send(`<h1>Something went wrong</h1>`);
-    } else {
-      res.write(data);
-      //* response end kora
-
-      //* The res.end() method ends the current response process. This method is used to quickly end the response without any data. If one needs to respond with data, they should use either the res.send() method or the res.json() method.
-      res.end();
-    }
-  });
-});
-app.get("/about", (req, res) => {
-  fs.readFile("./pages/about.html", (err, data) => {
-    if (err) {
-      console.log(`error`, err);
-      res.send(`<h1>something went wrong</h1>`);
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
-});
-app.get("/help", (req, res) => {
-  //console.log(req.app); // shows the whole express app
-  // console.log(req.hostname); // to check host name :localhost
-  // console.log(req.ip); // to check ip address
-  // console.log(req.method); // for checking methods
-  // console.log(req.originalUrl); // to see full url
-  // console.log(req.get("content-type")); // ki type er header asce
-  // console.log(req.get("accept")); // ki type er header asce
-
-  fs.readFile("./pages/help.html", (err, data) => {
-    if (err) {
-      console.log(`error`, err);
-      res.send(`<h1>something went wrong</h1>`);
-    } else {
-      // console.log(res.headersSent); // help us measure time and know if response sent or not
-      res.write(data);
-      res.end();
-      // console.log(res.headersSent);
-      // res.attachement(`filename`) // for attaching file
-      // res.append() // for combining http headers
-      // res.cookies() // for generating cookies
-      // res.clearCookies() // for clearing cooking
-      // res.download("./pages/dsa-cheatsheet.pdf"); // for dowloadable
-      // res.format({}) // for muliple content type jnno vinno vinno res pathate pari
-    }
-  });
-});
-app.get("/local", localM, (req, res) => {
-  res.send(`<h1>I am local route</h1>`);
-});
 //* server start
 app.listen(4000, () => {
   console.log(`server is listening`);
---
+++ router.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+router.get("/", (req, res) => {
+  //   res.send("hello world");
+  //   res.attachment() //for pdf or any other file
+  //   res.json({
+  //     message: "hello there!",
+  //   });
+  res.send(`<h1>hello from home</h1>`);
+});
+
+router.get("/pages", (req, res) => {
+  //console.log(req.url); // shows which endpoint user hit on the server
+  //* fs module read from the specified path and take a callback fn(err,data)
+  //* data:buffer is chunk of binary code
+  fs.readFile("./pages/index.html", (err, data) => {
+    if (err) {
+      console.log(`Error`, err);
+      res.send(`<h1>Something went wrong</h1>`);
+    } else {
+      res.write(data);
+      //* response end kora
+
+      //* The res.end() method ends the current response process. This method is used to quickly end the response without any data. If one needs to respond with data, they should use either the res.send() method or the res.json() method.
+      res.end();
+    }
+  });
+});
+router.get("/about", (req, res) => {
+  fs.readFile("./pages/about.html", (err, data) => {
+    if (err) {
+      console.log(`error`, err);
+      res.send(`<h1>something went wrong</h1>`);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+router.get("/help", (req, res) => {
+  //console.log(req.app); // shows the whole express app
+  // console.log(req.hostname); // to check host name :localhost
+  // console.log(req.ip); // to check ip address
+  // console.log(req.method); // for checking methods
+  // console.log(req.originalUrl); // to see full url
+  // console.log(req.get("content-type")); // ki type er header asce
+  // console.log(req.get("accept")); // ki type er header asce
+
+  fs.readFile("./pages/help.html", (err, data) => {
+    if (err) {
+      console.log(`error`, err);
+      res.send(`<h1>something went wrong</h1>`);
+    } else {
+      // console.log(res.headersSent); // help us measure time and know if response sent or not
+      res.write(data);
+      res.end();
+      // console.log(res.headersSent);
+      // res.attachement(`filename`) // for attaching file
+      // res.append() // for combining http headers
+      // res.cookies() // for generating cookies
+      // res.clearCookies() // for clearing cooking
+      // res.download("./pages/dsa-cheatsheet.pdf"); // for dowloadable
+      // res.format({}) // for muliple content type jnno vinno vinno res pathate pari
+    }
+  });
+});
+router.get("/local", (req, res) => {
+  res.send(`<h1>I am local route</h1>`);
+});
+module.exports = router;
